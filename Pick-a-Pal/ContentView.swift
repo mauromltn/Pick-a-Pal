4254586d02fd01b0a29d6e56f9174315722b65d1
mauromltn <mauromtne@icloud.com>
2025-01-02 15:44:15 +0100

    feat: Add all app features

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -1,13 +1,45 @@
 import SwiftUI
 
+
 struct ContentView: View {
 	@State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+	@State private var nameToAdd = ""
+	@State private var pickedName = ""
+	@State private var shouldRemovePickedName = false
 	
 	var body: some View {
 		VStack {
+			Text(pickedName.isEmpty ? " " : pickedName)
+			
 			List {
 				ForEach(names, id: \.description) { name in
 					Text(name)
+				}
+			}
+			
+			TextField("Add Name", text: $nameToAdd)
+				.autocorrectionDisabled()
+				.onSubmit {
+					if !nameToAdd.isEmpty {
+						names.append(nameToAdd)
+						nameToAdd = ""
+					}
+				}
+			
+			Divider()  // Adds a line between the TextField and the Button
+			
+			Toggle("Remove when picked", isOn: $shouldRemovePickedName)
+			
+			Button("Pick Random Name") {
+				if let randomName = names.randomElement() {
+					pickedName = randomName
+					
+					if shouldRemovePickedName {
+						names.removeAll { $0 == randomName }
+						// $0 represents the first argument passed
+					}
+				} else {
+					pickedName = ""
 				}
 			}
 		}
@@ -15,6 +47,8 @@ struct ContentView: View {
 	}
 }
 
+
 #Preview {
 	ContentView()
 }
+
